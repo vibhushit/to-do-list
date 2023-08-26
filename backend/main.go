75b53b7979716602f3e6dd63dad9271b65277b26
vibhushit <vibhushit10@gmail.com>
2023-08-26 23:15:30 +0530

    fontend integrated

--- backend/main.go
+++ backend/main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 	"to-do-list/todo"
 )
 
+var tmpl *template.Template
+var todoList *todo.ToDoList
+
 func main() {
-	fmt.Println("Welcome to the to-do list app...")
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/api/tasks", tasksAPIHandler)
+	http.HandleFunc("/api/add", addTaskAPIHandler)
 
-	//code
-	todoList := todo.NewToDoList()
+	tmpl = template.Must(template.ParseFiles("../frontend/index.html"))
 
-	todoList.AddTask("Buy Groceries")
-	todoList.AddTask("Write code")
-	todoList.CompleteTask(1)
+	todoList = todo.NewToDoList()
 
-	fmt.Println(todoList.ListTasks())
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
 
-	fmt.Println("Goodbye!")
+type ViewData struct {
+	Tasks []todo.Task
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	data := ViewData{
+		Tasks: todoList.ListTasks(),
+	}
+	tmpl.Execute(w, data)
+}
+
+func tasksAPIHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := todoList.ListTasks()
+	respondJSON(w, tasks)
+}
+
+func addTaskAPIHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	taskTitle := r.FormValue("title")
+	if taskTitle != "" {
+		todoList.AddTask(taskTitle)
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func respondJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	// You can use encoding/json to encode data and write it to the response writer.
 }
